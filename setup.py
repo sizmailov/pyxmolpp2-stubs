edb7f9c62591a1cf3c5d3543b2096f3947992f62
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-02-19 15:57:53 +0300

    bump version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     maintainer_email="sergei.a.izmailov@gmail.com",
     description="PEP 561 type stubs for pyxmolpp2",
     url="https://github.com/sizmailov/pyxmolpp2",
-    version="0.0.2",
+    version="0.0.3",
     packages=['pyxmolpp2-stubs'],
     cmdclass={
         'generate_stubs': GenerateStubsCommand,
